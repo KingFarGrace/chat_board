1ab86745418fda783a0df15815995035e8b18ded
凌墨羽 <1412098080@qq.com>
2021-06-11 10:55:37 +0800

    测试

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -3,8 +3,8 @@
 DIALECT = 'mysql'
 DRIVER = 'mysqldb'
 # 配置自己的用户名和密码
-USERNAME = ''
-PASSWORD = ''
+USERNAME = 'myj'
+PASSWORD = 'MYJ%DinnaMoyu2048'
 HOST = 'localhost'
 PORT = '3306'
 DATABASE = 'chat_board_sys'
