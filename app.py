528cf2dedeed6cdd56a893257b1166cc9582a1a2
wycmeepo <1825038989@qq.com>
2021-07-02 19:01:13 +0800

    csrf漏洞开发 -添加外部html发送请求（csrf_hack.html） -添加csrf保护 -更改index页面为登录页面

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from exts import db, app
 from views.router import router
 from models import user
 from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CsrfProtect
 
 app.register_blueprint(router)
 app.config.from_object(dbconfig)
 app.config.from_object(pdfconfilg)
+app.config["SECRET_KEY"] = '79537d00f4834892986f09a100aa1edf'
 db.init_app(app)
 bootstrap = Bootstrap(app)
 
@@ -16,4 +18,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
+    # csrf保护，所有表单前添加 <input type="hidden" name="csrf_token" value="{{ csrf_token() }}"> 即可
+    CsrfProtect(app)
     app.run(debug=True, port=5100)
